1cca7aa417ae8c2d870bb9253cb8c3bc3f0ae68c
humandavey <davey.adams.three@gmail.com>
2024-09-13 16:57:55 -0500

    Create project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# torque-hours-x
-An app written in flutter to manage Texas Torque members' hours.
+# torque_hours_x
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/torque_hours_x/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.torque_hours_x
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
